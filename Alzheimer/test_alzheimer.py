0ae333abde5b1c38e2d89d4cf03d365449a8aef2
jlokhande46 <jlokhande46@gmail.com>
2020-06-21 14:30:44 +0530

    Alzh model

---
+++ Alzheimer/test_alzheimer.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np 
+import pandas as pd
+from PIL import Image
+from keras.models import load_model
+import imutils
+
+
+model = load_model('alzheimer.hdf5')
+
+image = 'nonDem10.jpg'
+
+image = cv2.imread(image)
+image = cv2.resize(image,(176,176))
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+batch = np.expand_dims(image,axis=0)
+print(batch.shape)
+batch = np.expand_dims(batch,axis=3)
+print(batch.shape) 
+preds = model.predict(batch)
+
+print(preds)
